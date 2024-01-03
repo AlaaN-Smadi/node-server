52e69cd7326ac48f0e95ac92770f499ea604cacd
alaa smadi <alaasmadi1010@gmail.com>
2024-01-03 20:41:22 +0300

    add submissions count

--- server/server.js
+++ server/server.js
@@ -14,12 +14,13 @@ app.use(express.json());
 // Middleware for parsing URL-encoded payloads
 app.use(express.urlencoded({ extended: true }));
 let submissions = {};
+let submissionsCount = 0;
 
 // Set up API routes
 console.log("setupApiRoutes");
 app.get('/api/getSubmissions', async (req, res) => {
     try {
-        res.json(submissions);
+        res.json(submissions, submissionsCount);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Error occurred while sending data' });
@@ -27,6 +28,7 @@ app.get('/api/getSubmissions', async (req, res) => {
 });
 app.post('/api/addSubmissions', async (req, res) => {
     try {
+        submissionsCount += 1;
         submissions = req.body;
         res.json(submissions);
     } catch (error) {
@@ -36,7 +38,7 @@ app.post('/api/addSubmissions', async (req, res) => {
 });
 
 // Start the server
-const runServer = () =>{
+const runServer = () => {
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
